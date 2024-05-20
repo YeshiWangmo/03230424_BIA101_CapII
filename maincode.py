7217d841690b3ed55e980763c4695b4dd302e4fb
unknown <03230424.gcbs@rub.edu.bt>
2024-05-21 00:37:49 +0600

    cap2

--- maincode.py
+++ maincode.py
@@ -1,176 +1,206 @@
-
 class Person:
-    def __init__(self, name, age, marital_status, org_type=None, emp_type=None, salary=None, has_children=False, children_in_school=False, num_children_in_school=0):
+    def __init__(self, name, age, marital_status, organization_type, employee_type):
         self.name = name
         self.age = age
-
         self.marital_status = marital_status
-        self.org_type = org_type
-        self.emp_type = emp_type
-        self.salary = salary
-        self.has_children = has_children
-        self.children_in_school = children_in_school
-        self.num_children_in_school = num_children_in_school
+        self.organization_type = organization_type
+        self.employee_type = employee_type
 
 class Employee(Person):
-    def __init__(self, name, age, marital_status, org_type, emp_type, salary, has_children, children_in_school, num_children_in_school):
-        super().__init__(name, age, marital_status)
-        self.org_type = org_type
-        self.emp_type = emp_type
-        self.salary = salary
-        self.has_children = has_children
-        self.children_in_school = children_in_school
-        self.num_children_in_school = num_children_in_school
+    def __init__(self, name, age, marital_status, organization_type, employee_type, income, gis_contributions, pension_contributions, life_insurance_premium, self_education_allowance, donations, bonus_amount, rental_income=0, dividend_income=0, other_income=0):
+        super().__init__(name, age, marital_status, organization_type, employee_type)
+        self.income = income
+        self.gis_contributions = gis_contributions
+        self.pension_contributions = pension_contributions
+        self.life_insurance_premium = life_insurance_premium
+        self.self_education_allowance = self_education_allowance
+        self.donations = donations
+        self.bonus_amount = bonus_amount
+        self.rental_income = rental_income
+        self.dividend_income = dividend_income
+        self.other_income = other_income
+        self.num_children = 0
+        self.child_edu_allowances = []
+        self.sponsored_edu_expenses = []
+
+        if marital_status.lower() == "married":
+            self.num_children = self.get_num_children()
+            if self.num_children > 0:
+                self.child_edu_allowances = self.get_child_education_allowances()
+                self.sponsored_edu_expenses = self.get_sponsored_education_expenses()
+
+    def get_num_children(self):
+        while True:
+            try:
+                num_children_input = input("Do you have children? (yes/no): ")
+                if num_children_input.lower() == "yes":
+                    return int(input("Enter the number of your children: "))
+                elif num_children_input.lower() == "no":
+                    return 0
+                else:
+                    print("Please enter yes or no.")
+            except ValueError:
+                print(" Please enter a valid number of children.")
+
+    def get_child_education_allowances(self):
+        allowances = []
+        for child in range(self.num_children):
+            while True:
+                try:
+                    allowance = float(input(f"Enter education allowance for child {child + 1} (max Nu. 350,000): "))
+                    allowances.append(min(allowance, 350000))
+                    break
+                except ValueError:
+                    print("Please enter a valid number.")
+        return allowances
+
+    def get_sponsored_education_expenses(self):
+        expenses = []
+        for child in range(self.num_children):
+            while True:
+                goes_to_school = input(f"Does your child {child + 1} go to school? (y/n): ").lower()
+                if goes_to_school in ['y', 'n']:
+                    break
+                print("There is no such option,  check the spelling and retype it again.")
+
+            if goes_to_school == "y":
+                while True:
+                    try:
+                        expense = float(input(f"Enter your sponsored education expense for your child {child + 1} (max Nu. 350,000): "))
+                        expenses.append(min(expense, 350000))
+                        break
+                    except ValueError:
+                        print(" Please enter a valid number.")
+            else:
+                expenses.append(0)
+        return expenses
 
 class TaxCalculator:
     def __init__(self, employee):
         self.employee = employee
-        self.pit = self.calculate_pit()
-        self.surcharge = self.calculate_surcharge()
-        self.total_tax = self.pit + self.surcharge
-        self.bonus = self.calculate_bonus()
-        self.provident_fund = self.calculate_provident_fund()
-    
-    def calculate_rental_tax(self):
-        if self.employee.rental_income is None or self.employee.rental_expenses is None:
-            return 0
 
-        taxable_rental_income = max(0, self.employee.rental_income - self.employee.rental_expenses)
-        rental_tax_rate = 0.1  # Assuming a flat rate of 10% for simplicity
+    def calculate_tax(self):
+        total_income = self.employee.income
+    #Deduction
+        total_income -= self.employee.gis_contributions
+        total_income -= self.employee.pension_contributions
 
-        return taxable_rental_income * rental_tax_rate
-    
+        rental_income = self.employee.rental_income * 0.8  # 20% deduction on repairs and maintenance, etc.
+        total_income += rental_income
 
-    def calculate_pit(self):
-        if self.employee.age < 18:
-            return 0
-        if self.employee.salary < 300000:
-            return 0
-
-        tax_brackets = [
-            (300000, 400000, 0.1),
-            (400001, 650000, 0.15),
-            (650001, 1000000, 0.2),
-            (1000001, 1500000, 0.25),
-            (1500001, float('inf'), 0.3)
-        ]
-
-        pit = 0
-        remaining_income = self.employee.salary
-
-        for lower, upper, rate in tax_brackets:
-            if remaining_income <= lower:
-                break
-            if remaining_income > upper - lower:
-                pit += (upper - lower) * rate
-                remaining_income -= upper - lower
-            else:
-                pit += remaining_income * rate
-                remaining_income = 0
-
-        return pit
-
-    def calculate_surcharge(self):
-        if self.pit >= 1000000:
-            return self.pit * 0.1
+        # 10 %  of TDS
+        if self.employee.dividend_income > 30000:
+            dividend_income = (self.employee.dividend_income - 30000) * 0.9  
         else:
-            return 0
-
-    def calculate_bonus(self):
-        if self.employee.emp_type == 'Regular':
-            bonus_rate = 0.1  # 10% bonus will be given to regular employees
-        else:
-            bonus_rate = 0.05  # 5% bonus will be given to contract employees
-        bonus = self.employee.salary * bonus_rate
-        return bonus
-
-    def calculate_provident_fund(self):
-        provident_fund = 0
-        if self.employee.emp_type == 'Regular':
-            if self.employee.org_type == 'Government':
-                provident_fund_rate = 0.05  # 5% will be given to regular employees of government organizations
-            else:
-                provident_fund_rate = 0.1  # 10% will be given to regular employees of private/corporate organizations
-            provident_fund = self.employee.salary * provident_fund_rate
-        elif self.employee.org_type != 'Government':
-            provident_fund_rate = 0.05  # 5% will be given to contract employees of private/corporate organizations
-            provident_fund = self.employee.salary * provident_fund_rate
-        return provident_fund
-
-class Deductions:
-    def __init__(self, employee):
-        self.employee = employee
-        self.deductions = self.calculate_deductions()
-
-    def calculate_deductions(self):         
-        deductions = 0
-        if self.employee.emp_type == 'Regular':
-            if self.employee.org_type == 'Government':
-                deductions += min(self.employee.salary * 0.1, 350000)  # NPPF
-            else:
-                deductions += min(self.employee.salary * 0.1, 350000)  # NPPF
-            deductions += 200 * 12  # GIS (200 per month)
-
-        if self.employee.marital_status and self.employee.has_children:
-            if self.employee.children_in_school:
-                deductions += min(350000 * self.employee.num_children_in_school, 350000 * 5)  # Education allowance
-            else:
-                deductions += 350000  # Education allowance for a child
-
-        deductions += min(350000, self.employee.salary * 0.05)  # Donations
-        deductions += min(self.employee.salary * 0.1, 350000)  # Life insurance premium
-        deductions += min(350000, self.employee.salary * 0.05)  # Self-education allowance
-
-        return deductions
-
-def calculate_tax():
-    name = input("Enter your name: ")
-    try:
-        age = int(input("Enter your age: "))
-    except ValueError:
-        print('Please enter a number')
-        exit()
-
-    if age < 18:
-        print("You are below 18 years of age, so you don't need to pay any tax.")
-    else:
-        marital_status = input("Are you married? (Yes/No): ").lower() == 'yes'
-        org_type = input("Enter your organization type (Government/Private/Corporate): ").lower()
-        emp_type = input("Enter your employee type (Regular/Contract): ").lower()
-        salary = int(input("Enter your salary: "))
-        has_children = input("Do you have children? (Yes/No): ").lower() == 'yes'
-        children_in_school = input("Are your children going to school? (Yes/No): ").lower() == 'yes'
-        num_children_in_school = int(input("Enter the number of children going to school: ")) if children_in_school else 0
-
-        person = Person(name, age, marital_status)
-        employee = Employee(person.name, person.age, person.marital_status, org_type, emp_type, salary, has_children, children_in_school, num_children_in_school)
-        deductions = Deductions(employee)
-        tax_calculator = TaxCalculator(employee)
+            dividend_income = self.employee.dividend_income
+        total_income += dividend_income
         
+        #30% deduction on other income
+        other_income = self.employee.other_income * 0.7 
+        total_income += other_income
 
-        print(f"Name: {employee.name}")
-        print(f"Organization Type: {employee.org_type}")
-        print(f"Employee Type: {employee.emp_type}")
-        print(f"Age: {employee.age}")
-        print(f"Marital Status: {'Married' if employee.marital_status else 'Single'}")
-        print(f"Has Children: {'Yes' if employee.has_children else 'No'}")
+        total_income -= sum(self.employee.child_edu_allowances)
+        total_income -= self.employee.life_insurance_premium
+        total_income -= min(self.employee.self_education_allowance, 350000)
 
-        if employee.has_children:
-            print(f"Children in School: {'Yes' if employee.children_in_school else 'No'}")
-            if employee.children_in_school:
-                print(f"Number of Children in School: {num_children_in_school}")
+        #checking whether donation exceed 5% of the incomr
+        max_donation = 0.05 * total_income
+        if self.employee.donations > max_donation:
+            print(f"Note: Donations are limited to {max_donation}. Excess amount will not be considered for deduction.")
+            donations = max_donation
+        else:
+            donations = self.employee.donations
 
-        print(f"Salary: {employee.salary}")
-        print(f"Deductions: {deductions.deductions}")
-        print(f"Personal Income Tax (PIT): {tax_calculator.pit}")
-        print(f"Surcharge: {tax_calculator.surcharge}")
-        print(f"Total Tax Payable: {tax_calculator.total_tax}")
-        print(f"Bonus: {tax_calculator.bonus}")
-        print(f"Provident Fund: {tax_calculator.provident_fund}")
+        total_income -= donations
+        total_income -= sum(self.employee.sponsored_edu_expenses)
+        total_income -= self.employee.bonus_amount
 
-    
-    
+        return self.apply_tax_slabs(total_income)
 
+    def apply_tax_slabs(self, total_income):
+        tax_amount = 0
+        if total_income <= 300000:
+            tax_amount = 0
+        elif total_income <= 400000:
+            tax_amount = (total_income - 300000) * 0.1
+        elif total_income <= 650000:
+            tax_amount = (total_income - 400000) * 0.15 + 10000
+        elif total_income <= 1000000:
+            tax_amount = (total_income - 650000) * 0.2 + 45500
+        elif total_income <= 1500000:
+            tax_amount = (total_income - 1000000) * 0.25 + 130500
+        else:
+            tax_amount = (total_income - 1500000) * 0.3 + 280500
+
+        if total_income >= 1000000:
+            tax_amount += tax_amount * 0.1
+
+        return tax_amount
+
+def main():
+    while True:
+        print("Personal Income Tax (PIT) Calculator")
+        name = input("Enter your name: ")
+
+        while True:
+            try:
+                age = int(input("Enter your age: "))
+                if age > 100:
+                    print("Enter your age correctly.")
+                    continue
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+        if age < 18:
+            print("Since you are below 18 years old. You are not required to pay taxes.")
+            continue
+
+        while True:
+            marital_status = input("Enter your marital status (Married/Single): ").lower()
+            if marital_status in ["married", "single"]:
+                break
+            else:
+                print("There is no such option, check the spelling and retype it again.")
+
+        while True:
+            organization_type = input("Enter the organization type (Government/Private/Corporate): ").lower()
+            if organization_type in ["government", "private", "corporate"]:
+                break
+            else:
+                print("There is no such option, make sure you check the spelling and retype it again.")
+
+        while True:
+            employee_type = input("Enter the employee type (Regular/Contract): ").lower()
+            if employee_type in ["regular", "contract"]:
+                break
+            else:
+                print("There is no such option, make sure you check the spelling and retype it again.")
+
+        while True:
+            try:
+                income = float(input("Enter your annual gross salary: "))
+                gis_contributions = float(input("Enter your GIS contributions: "))
+                pension_contributions = float(input("Enter your pension contributions (NPPF/PF): ")) if organization_type!= "government" and employee_type == "contract" else 0
+                life_insurance_premium = float(input("Enter your life insurance premium: "))
+                self_education_allowance = float(input("Enter your self-education allowance, if any: ") or 0)
+                donations = float(input("Enter your donations (if any): ") or 0)
+                bonus_amount = float(input("Enter your bonus amount: ") or 0)
+                rental_income = float(input("Enter your annual rental income (if any): ") or 0)
+                dividend_income = float(input("Enter your annual dividend income (if any): ") or 0)
+                other_income = float(input("Enter your annual income from other sources (if any): ") or 0)
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+        employee = Employee(name, age, marital_status, organization_type, employee_type, income, gis_contributions, pension_contributions, life_insurance_premium, self_education_allowance, donations, bonus_amount, rental_income, dividend_income, other_income)
+        calculator = TaxCalculator(employee)
+        tax_amount = calculator.calculate_tax()
+        print(f"{employee.name}'s tax amount is: {tax_amount}")
+
+        run_again = input("Would you like to calculate tax again? (yes/no): ").lower()
+        if run_again!= "yes":
+            break
 
 if __name__ == "__main__":
-    calculate_tax()
+    main()
